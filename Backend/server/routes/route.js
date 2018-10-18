cd5836c96958a810b0711fa0964464b80d6dde64
root <root@ITEM-S68727.emea.msad.sopra>
2018-10-18 17:53:17 +0200

    Ajout serveur de base

---
+++ Backend/server/routes/route.js
@@ -0,0 +1,24 @@
+let Client = require('node-rest-client').Client;
+let options ={  
+    mimetypes:{
+        json:["application/json","application/json; charset=utf-8"]
+    }
+};
+
+let client = new Client(options);
+
+
+let appRouter = function (app) {
+
+    app.get("/", function(req, res) {
+        res.status(200).send("Welcome to our restful API");
+    });
+
+    app.use(function(req, res){
+        res.setHeader('Content-Type', 'text/plain');
+        res.status(404).send('Page introuvable !');
+    });
+
+};
+
+module.exports = appRouter;
