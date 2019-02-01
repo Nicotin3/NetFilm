408ed1bdeb72554920e54385e4f36e62ad02686e
Will Djbs <will.djbs@gmail.com>
2019-02-01 12:52:14 +0100

    Derniere version front

--- Frontend/NetFilmApp/src/app/app-routing.module.ts
+++ Frontend/NetFilmApp/src/app/app-routing.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {FilmsComponent} from './films/films.component';
+import {SearchComponent} from './search/search.component';
+import {FilmComponent} from './film/film.component';
+import {AddComponent} from './add/add.component';
 
 const routes: Routes = [
   {
     path : '',
-    component : FilmsComponent
+    component : SearchComponent
+  },
+  {
+    path : 'add',
+    component : AddComponent
+  },
+  {
+    path : 'film/:id',
+    component : FilmComponent
   },
   {
     path : 'films',
     component : FilmsComponent
-  },
+  }
 ];
 
 @NgModule({
---
+++ Frontend/NetFilmApp/src/app/search/search.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl} from '@angular/forms';
+import {DataService} from '../data.service';
+import {Observable} from 'rxjs/index';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+
+  title = new FormControl('');
+  genre = new FormControl('');
+  autor = new FormControl('');
+  actor = new FormControl('');
+
+  page: Number = 0;
+  films$: any[] ;
+  totalRec: Number = 10;
+
+  constructor(private  data: DataService) { }
+
+  ngOnInit() {
+  }
+
+  searchFilms(): void {
+   // this.films$ = this.data.searchFilms(this.page, this.title.value, this.genre.value, this.autor.value, this.actor.value);
+    const ttl: String = ' ' + this.title.value;
+    const ge: String = ' ' + this.genre.value;
+    const aut: String = ' ' + this.autor.value;
+    const act: String = ' ' + this.actor.value;
+    this
+      .data
+      .searchFilms(this.page, ttl, ge, aut, act)
+      .subscribe((resp: Object) => {
+        this.films$ = resp.data;
+        this.totalRec = resp.count;
+        console.log(this.totalRec);
+        console.log(this.page);
+      });
+  }
+
+
+}
